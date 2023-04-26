10363b230ba080c15db3ec382866041f80247cf6
sangminlee98 <tkdals0975@naver.com>
2023-04-27 03:12:44 +0900

    [FEAT] : 사용자 로그인 상태에 따른 페이지 리다이렉트 처리

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
+import { Navigate, useOutletContext } from 'react-router-dom';
 
 export default function SignInPage() {
+  const authState = useOutletContext();
+
+  if (authState) {
+    return <Navigate to="/todo" />;
+  }
   return <div>로그인페이지</div>;
 }
--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
+import { Navigate, useOutletContext } from 'react-router-dom';
 
 export default function SignUpPage() {
+  const authState = useOutletContext();
+
+  if (authState) {
+    return <Navigate to="/todo" />;
+  }
   return <div>회원가입페이지</div>;
 }
