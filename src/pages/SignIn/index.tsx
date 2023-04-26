5f786587a206693b35c079b8cf4cb78bd641cbba
rjsej12 <rjsej12@naver.com>
2023-04-27 03:35:06 +0900

    [FEAT] : SignIn, SignUp 페이지에 AuthForm 연결

---
+++ src/pages/SignIn/index.tsx
@@ -0,0 +1,5 @@
+import AuthForm from '@/components/AuthForm';
+
+export default function SignInPage() {
+  return <AuthForm formtype="signin" />;
+}
---
+++ src/pages/SignUp/index.tsx
@@ -0,0 +1,5 @@
+import AuthForm from '@/components/AuthForm';
+
+export default function SignUpPage() {
+  return <AuthForm formtype="signup" />;
+}
