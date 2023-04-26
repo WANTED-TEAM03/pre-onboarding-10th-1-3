04acf199d77ed59374ec00c8a6655de13e6633c9
sangminlee98 <tkdals0975@naver.com>
2023-04-27 02:59:02 +0900

    [STYLE] : reset.scss, media.scss 전역 스타일 코드 추가

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import '@/styles/reset.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
