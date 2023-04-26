321d8ea9ced95700eb00b4cea643d0e4934c8653
rjsej12 <rjsej12@naver.com>
2023-04-27 03:28:55 +0900

    [FEAT] : useInput custom hook 추가

---
+++ src/hooks/useInput.ts
@@ -0,0 +1,39 @@
+import React, { useRef, useState } from 'react';
+
+type Validator = {
+  isValid: boolean;
+  message?: string;
+};
+
+type UseInputProps = {
+  initValue: string;
+  validator?: (value: string) => Validator;
+};
+
+type UseInputReturn = [
+  {
+    value: string;
+    onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+    setValue: React.Dispatch<React.SetStateAction<string>>;
+  },
+  Validator,
+];
+
+const useInput = ({ initValue, validator }: UseInputProps): UseInputReturn => {
+  const [value, setValue] = useState(initValue);
+  const validationResult = useRef<Validator>({ isValid: false });
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { value: InputValue } = e.target;
+
+    if (typeof validator === 'function') {
+      validationResult.current = validator(InputValue);
+    }
+
+    setValue(InputValue);
+  };
+
+  return [{ value, onChange, setValue }, validationResult.current];
+};
+
+export default useInput;
