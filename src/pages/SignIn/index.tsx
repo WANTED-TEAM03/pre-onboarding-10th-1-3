7276ca6f20f6d33385448b86634755d229749ff6
pySoo <soopy368@gmail.com>
2023-04-27 14:30:20 +0900

    [FEAT] : 로그인 성공 시 /todo 이동

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,11 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Navigate, useOutletContext } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import { SignInAPI } from '@/services/auth';
+import { AuthForm } from '@/types/authForm';
 
 export default function SignInPage() {
+  const [email] = useState('');
+  const [password] = useState('');
+
+  const navigate = useNavigate();
+
+  const handleSubmit = async () => {
+    const authForm: AuthForm = {
+      email,
+      password,
+    };
+
+    await SignInAPI(authForm).then(response => {
+      navigate('/todo');
+    });
+  };
+
   const authState = useOutletContext();
 
   if (authState) {
     return <Navigate to="/todo" />;
   }
-  return <div>로그인페이지</div>;
+  return (
+    <div>
+      <button type="button" data-testid="signin-button" onClick={handleSubmit}>
+        로그인
+      </button>
+    </div>
+  );
 }
