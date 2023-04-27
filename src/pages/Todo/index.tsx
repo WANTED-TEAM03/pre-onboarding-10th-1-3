9c0e4334b94662781ff26d8984f0f694b4932f38
myungju <myungju030@gmail.com>
2023-04-28 04:09:20 +0900

    [FEAT] : 새로운 todo 추가 기능 구현

--- src/pages/Todo/index.tsx
+++ src/pages/Todo/index.tsx
@@ -1,23 +1,49 @@
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 import { Navigate, useOutletContext } from 'react-router-dom';
 import AddTodoInput from '@/components/AddTodoInput';
 import { TodoItem } from '@/components/TodoItem';
 import { ROUTE_PATHS } from '@/constants/config';
+import useInput from '@/hooks/useInput';
+import { createTodoAPI } from '@/services/todo';
 import styles from './styles.module.scss';
 
 export default function TodoPage() {
   const isLoggedIn = useOutletContext();
   const listRef = useRef<HTMLUListElement>(null);
+  const [todos, setTodos] = useState<TodoType[]>([]);
+  const [todoInput] = useInput({
+    initValue: '',
+  });
 
   if (!isLoggedIn) {
     return <Navigate to={ROUTE_PATHS.signIn} />;
   }
 
+  const scrollToBottom = () => {
+    listRef.current?.scrollTo({
+      top: listRef.current.scrollHeight,
+      behavior: 'smooth',
+    });
+  };
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (todoInput.value === '') return;
+    const response = await createTodoAPI(todoInput.value);
+    if (response) {
+      setTodos(list => [...list, response]);
+    }
+    scrollToBottom();
+    todoInput.setValue('');
+  };
+
   return (
     <div className={styles.pageWrapper}>
-      <AddTodoInput />
+      <AddTodoInput value={todoInput.value} onChange={todoInput.onChange} onSubmit={handleSubmit} />
       <ul ref={listRef} className={styles.listWrapper}>
-        <TodoItem />
+        {todos.map(todo => (
+          <TodoItem key={todo.id} />
+        ))}
       </ul>
     </div>
   );
