f0fb4fa8ee9e2b472ad0f6a17aec9f234cbb0061
Chuhj <chj9574@naver.com>
2023-04-28 13:12:06 +0900

    [FEAT] : handleUpdateTodo 추가

--- src/pages/Todo/index.tsx
+++ src/pages/Todo/index.tsx
@@ -1,13 +1,21 @@
-import { useRef } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 import { Navigate, useOutletContext } from 'react-router-dom';
 import AddTodoInput from '@/components/AddTodoInput';
 import { TodoItem } from '@/components/TodoItem';
 import { ROUTE_PATHS } from '@/constants/config';
+import { updateTodoAPI } from '@/services/todo';
 import styles from './styles.module.scss';
 
 export default function TodoPage() {
   const isLoggedIn = useOutletContext();
   const listRef = useRef<HTMLUListElement>(null);
+  const [todos, setTodos] = useState<TodoType[]>([]);
+
+  const handleUpdateTodo = useCallback(async (id: number, todo: string, isCompleted: boolean) => {
+    if (todo === '') return alert('내용을 입력해주세요.');
+    const updatedTodo = await updateTodoAPI(id, todo, isCompleted);
+    setTodos(prevTodos => prevTodos.map(prevTodo => (prevTodo.id === id ? updatedTodo : prevTodo)));
+  }, []);
 
   if (!isLoggedIn) {
     return <Navigate to={ROUTE_PATHS.signIn} />;
@@ -17,7 +25,9 @@ export default function TodoPage() {
     <div className={styles.pageWrapper}>
       <AddTodoInput />
       <ul ref={listRef} className={styles.listWrapper}>
-        <TodoItem />
+        {todos.map(todo => (
+          <TodoItem key={todo.id} todoItem={todo} handleUpdateTodo={handleUpdateTodo} />
+        ))}
       </ul>
     </div>
   );
