e4952d79ce4aa91de03119235a7f9e431fa5e7ad
Chuhj <chj9574@naver.com>
2023-04-28 13:17:09 +0900

    [FEAT] : 오토 포커스 추가

--- src/components/TodoItem/index.tsx
+++ src/components/TodoItem/index.tsx
@@ -1,31 +1,50 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+import useInput from '@/hooks/useInput';
 import styles from './styles.module.scss';
 
-export function TodoItem() {
-  const [isUpdating] = useState(false);
+type TodoItemProps = {
+  todoItem: TodoType;
+  handleUpdateTodo: (id: number, todoItem: string, isCompleted: boolean) => Promise<void>;
+};
+
+export function TodoItem({ todoItem, handleUpdateTodo }: TodoItemProps) {
+  const [isEditing, setIsEditing] = useState(false);
+  const [editInput] = useInput({ initValue: todoItem.todo });
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    if (isEditing) inputRef.current?.focus();
+  }, [isEditing]);
+
   return (
     <li className={styles.todoWrapper}>
       <input type="checkbox" className={styles.todoCheckbox} />
-      {isUpdating ? (
-        <div className={styles.inputWrapper}>
-          <input className={styles.updateInput} data-testid="modify-input" />
+      {isEditing ? (
+        <form className={styles.inputWrapper}>
+          <input
+            data-testid="modify-input"
+            className={styles.updateInput}
+            value={editInput.value}
+            onChange={editInput.onChange}
+            ref={inputRef}
+          />
           <div className={styles.buttonWrapper}>
-            <button type="button" data-testid="submit-button">
+            <button data-testid="submit-button" type="submit">
               제출
             </button>
-            <button type="button" data-testid="cancel-button">
+            <button data-testid="cancel-button" type="button">
               취소
             </button>
           </div>
-        </div>
+        </form>
       ) : (
         <div className={styles.inputWrapper}>
-          <p className={styles.todo} />
+          <p className={styles.todo}>{todoItem.todo}</p>
           <div className={styles.buttonWrapper}>
-            <button type="button" data-testid="modify-button">
+            <button data-testid="modify-button" type="button" onClick={handleEditTodo}>
               수정
             </button>
-            <button type="button" data-testid="delete-button">
+            <button data-testid="delete-button" type="button">
               삭제
             </button>
           </div>
